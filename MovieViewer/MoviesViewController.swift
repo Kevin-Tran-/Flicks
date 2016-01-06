9cfb49fcf5b16a8349baf45a9cb481c4410ec383
Kevin-Tran- <tranpd88@gmail.com>
2016-01-05 17:53:13 -0800

    added image and pods

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AFNetworking
+
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -63,7 +65,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
         
+        let baseURL = "http://image.tmdb.org/t/p/w500"
+        
+        let imageURL = NSURL(string: baseURL + posterPath)
+        
+        cell.posterView.setImageWithURL(imageURL!)
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         
